4a2445116ba225f9a75edd3a5c86b30dd603482c
JuelDiCamillo <manuel.boldt1@googlemail.com>
2018-05-16 18:36:21 +0200

    bitteschön

---
+++ app.js
@@ -0,0 +1,5 @@
+const playerForm = document.getElementById('playerForm');
+const jsonResponseDiv = document.getElementById('jsonResponseDiv');
+const apiKey = "";
+const apiUrl = "https://api.playbattlegrounds.com/shards/pc-eu/";
+
