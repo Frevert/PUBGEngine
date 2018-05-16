a9c475f3da4cb6082c158a92948420bb1104daa8
Michael Frevert <michael-frevert@gmx.de>
2018-05-16 18:52:25 +0200

    Api Key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const playerForm = document.getElementById('playerForm');
 const jsonResponseDiv = document.getElementById('jsonResponseDiv');
-const apiKey = "";
+const apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyNWNiM2I3MC0xOGQxLTAxMzYtYzdlZi0wOWIxZjFlY2ZhOGQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNjkzNTMwLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Imlkb250a25vdyIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.O8_qdKIA9JurJ3cOGmOj7IrVtblAPVZ_MeJ75z4wRHY";
 const apiUrl = "https://api.playbattlegrounds.com/shards/pc-eu/";
 
